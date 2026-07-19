81bad918da8fa904ee64bb55aa6084a19d6a0021
AhmedLouay-coder21 <140275085+AhmedLouay-coder21@users.noreply.github.com>
2026-07-19 19:07:39 +0300

    Initial Commit
    -update .gtignore

---
+++ Controllers/ShiftsControllerController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShiftsLogger.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ShiftsLoggerController : ControllerBase
+    {
+    }
+}
