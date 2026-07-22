dae430b7d230688e26b724c183baa7d865f2bdf1
AhmedLouay-coder21 <140275085+AhmedLouay-coder21@users.noreply.github.com>
2026-07-22 21:33:41 +0300

    feat(backend): implement model validation using data annotations
    - decorate Employee and Shift models with validation attributes
    - enforce required properties and value constraints
    - initialize non-nullable string properties

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,13 +1,24 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace ShiftsLogger.Models
 {
     public class Employee
     {
         public int Id { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public string? Role { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string? FirstName { get; set; } = "";
+
+        [Required]
+        [MaxLength(50)]
+        public string? LastName { get; set; } = "";
+
+        [Required]
+        public string? Role { get; set; } = "";
+
+        [Range(0, 100000)]
         public int? Salary { get; set; }
 
         [JsonIgnore]
--- Models/Shift.cs
+++ Models/Shift.cs
@@ -1,4 +1,6 @@
-﻿namespace ShiftsLogger.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShiftsLogger.Models
 {
     public class Shift
     {
@@ -9,12 +11,19 @@
         public int EmployeeId { get; set; }
         // shift time span (which should be calculated by StartDate - EndDate
         public TimeSpan Duration => EndDate - StartDate;
+
         // shift area
-        public string? Area { get; set; }
+        [Required]
+        public string? Area { get; set; } = "";
+
+        [Required]
         // shift start date
         public DateTime StartDate { get; set; }
+
+        [Required]
         // shift end date
         public DateTime EndDate { get; set; }
+
         // foreign key
         public Employee Employee { get; set; } = null!;
     }
